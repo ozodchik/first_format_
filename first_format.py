732e4e440d9983c0a035f06c402d195a98fe0b74
Ozodchik <dilovardehkanov730@gmail.com>
2020-10-18 14:45:02 +0300

    last commited

--- first_format.py
+++ first_format.py
@@ -1,9 +1,12 @@
 import json
 from collections import Counter
+
+
 def get_words(filename):
   results = []
-  with open(filename, encoding="utf-8") as first_json:    data = json.load(first_json)
-  first_example = data["rss"]["channel"]["items"]
+  with open(filename, encoding="utf-8") as first_json:    
+    data = json.load(first_json)
+    first_example = data["rss"]["channel"]["items"]
   for result in first_example:
     new_result = result["description"].lower().split()
     for new in new_result:
