9894f170425138d150b06329e580b8c6cb7104da
Ozodchik <dilovardehkanov730@gmail.com>
2020-10-19 12:07:56 +0300

    commited homework

--- first_format.py
+++ first_format.py
@@ -2,20 +2,21 @@ import json
 from collections import Counter
 
 
-def get_words(filename):
-  results = []
-  with open(filename, encoding="utf-8") as first_json:    
-    data = json.load(first_json)
-    first_example = data["rss"]["channel"]["items"]
-  for result in first_example:
-    new_result = result["description"].lower().split()
-    for new in new_result:
-        if len(new) > 6:
-            results.append(new)
- 
-  last_list = Counter(results)
-  sorted_list =  last_list.most_common(10)
-  return f'топ 10 самые часто используемые слова:{sorted_list}'
-  
-print(get_words("newsafr.json"))
+def get_words(filename: str, top_size: int, min_word_len: int) -> str:
+    all_words = []
 
+    with open(filename, encoding="utf-8") as first_json:
+        data = json.load(first_json)
+        news = data["rss"]["channel"]["items"]
+
+    for article in news:
+        words = article["description"].lower().split()
+        for word in words:
+            if len(word) > min_word_len:
+                all_words.append(word)
+
+    counter = Counter(all_words)
+    top_list = counter.most_common(top_size)
+    return f"топ {top_size} самые часто используемые слова:{top_list}"
+
+print(get_words("newsafr.json", top_size=10, min_word_len=6))
